7976905bad381a5ce3da54e470be04ae6ffae86c
los bagos <158247609+losbagos@users.noreply.github.com>
2025-02-05 20:40:06 +0800

    Update main.py
    i did stuff

--- main.py
+++ main.py
@@ -1,32 +1,35 @@
 ## total mailbox death - ,  .  :3
 
-## a losbagos . d 
+## a losbagos . d https://github.com/losbagos
+# consider donating!!
+# XMR/Monero: 432cg1Mhzg4esHifrrifvsgQYWHHwsceDEoK79gvVmMENuhge72b53q8m5ZNHE3f8U5ogpJPA2gYfM7EV85ikzC6SvciivS
 
-#           >>>>> >>>>><<<<< <<<<<
+from math import trunc
+import random
 
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+
+from settings import *
+import sys
 import time
+
 import tkinter
 import tkinter as tk
 from tkinter import ttk
-import sys
-from math import trunc
-
-from settings import *
 
 
-#          >>>>> >>>>><<<<< <<<<<
-
-# -- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ definitions ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ -- 
+#    ⚘ ᠃ -- definitions
  
 options = Options()
-driver = webdriver.Chrome()
-wait = WebDriverWait(driver,5)
+driver = webdriver.Chrome() # replace this with ur preferred driver
+options.page_load_strategy = 'eager'
+driver = webdriver.Chrome(options=options)
 
+wait = WebDriverWait(driver,5)
 
 
 prefs = {
@@ -57,12 +60,14 @@ def tmdGoob():
 
 
 ## ˋ°•*⁀➷ shnooie for all email element variants
+
 def tmdShnooie_email(sn_Email):
     try: 
         driver.implicitly_wait(1)
-        sn_Email_element = driver.find_element(By.NAME, sn_Email)
+        sn_Email_element = driver.find_element(By.NAME, sn_Email) # goes through list and finds the name element that matches
         sn_Email_element.send_keys(shnooEmail)
     except:
+            print("error!! couldn't find the 'name' element for 'email'") # please send me this error if you see it, that means a website is there when it shouldn't be
             pass # will pass any lieek umm ones it can't find
 
 ## ˋ°•*⁀➷ shnooie for all first name element variants
@@ -72,6 +77,7 @@ def tmdShnooie_firstName(sn_firstName):
         sn_Email_element = driver.find_element(By.NAME, sn_firstName)
         sn_Email_element.send_keys(shnooFirstname)
     except:
+            print("error!! couldn't find the 'name' element for 'first name'")
             pass
     finally:
         time.sleep(1)
@@ -82,11 +88,12 @@ def tmdShnooie_lastName(sn_lastname):
         sn_Email_element = driver.find_element(By.NAME, sn_lastname)
         sn_Email_element.send_keys(shnooLastname)
     except:
+            print("error!! couldn't find the 'name' element for 'last name'")
             pass
     finally:
         time.sleep(1)
 
-# -- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ -- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ --  
+# -- ᠃ ⚘ finding email input ⚘ ᠃ --
 
 emaillist = ["email", "EMAIL", "contact[email]"]
 
@@ -95,7 +102,7 @@ def tmdSnorrey_email():
         tmdShnooie_email(i)
     
 
-# -- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ -- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ --  
+# -- ᠃ ⚘ finding first name input ⚘ ᠃ --
 
 firstNamelist = ["name", "NAME", "first_name", "FNAME", "firstname"]
 
@@ -103,7 +110,7 @@ def tmdSnorrey_firstName():
     for i in firstNamelist:
         tmdShnooie_firstName(i)
 
-# -- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ -- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ --  
+#  -- ᠃ ⚘ finding last time input ⚘ ᠃ --
 
 lastNamelist = ["lastname", "LNAME", "last_name"]
 
@@ -111,12 +118,14 @@ def tmdSnorrey_lastName():
     for i in lastNamelist:
         tmdShnooie_lastName(i)
 
-# -- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ turning list.txt into a list ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ --  
-
+#  -- ᠃ ⚘ list.txt into a list and randomizing a bit ⚘ ᠃ --
 my_file = open("list.txt", "r")  # list of all da urls n shiet
 data = my_file.read() 
 websitesList = data.split("\n")   
-# -- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ opening da website ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ -- 
+random.shuffle(websitesList)
+
+
+# -- ᠃ ⚘ opening da website ⚘ ᠃ --
 
 def tmdWebsite(website):
     driver.get(website)
@@ -126,7 +135,7 @@ def tmdWebsite(website):
     tmdSnorrey_lastName()
     time.sleep(1)
 
-# -- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ -- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ -- 
+# -- ᠃ ⚘ entering link ⚘ ᠃ --
 
 step = trunc(100/len(websitesList))
 
@@ -138,10 +147,15 @@ def tmdWeb():
         progress.step(step)
         progress.update()
 
+# -- ᠃ ⚘ exit button ⚘ ᠃ --
 
+def tmdExit():
+     driver.close() 
+     sys.exit()
         
+# https://github.com/losbagos
 
-# -- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ -App- ᠃ ⚘᠂ ⚘ ˚ ⚘ ᠂ ⚘ ᠃ -- 
+# -- ᠃ ⚘ application thingy ⚘ ᠃ --
 
 tmdEmail_label = tkinter.Label(tmdApp, text="Email Address", background=LightPink)
 tmdName_label = tkinter.Label(tmdApp, text="First Name", background=LightPink )
@@ -154,23 +168,7 @@ tmdLastName_entry = tkinter.Entry(tmdApp)
 tmdSubmit_label = tkinter.Label(tmdApp, text="Total Mailbox Death!", background=LightPink)
 tmdSubmit_button = tkinter.Button (tmdApp, text="run newsletter spammer", background=Pink, command = tmdGoob)
 
-# TO DO, FIGURE OUT !!!!
-
-def tmdExit():
-    driver.close()
-    sys.exit("bai bai")
-
-
-    
-
-tmdExit_button = tkinter.Button(tmdApp, text="Quit Script :3", background=Pink, command=tmdExit)
-
-
-
-                             
-
-
-
+tmdExit_button = tkinter.Button(tmdApp, text="quit script :3", background=Pink, command=tmdExit)
 
 tmdSubmit_label.grid(row=0, column=0, columnspan=2)
 
@@ -187,12 +185,11 @@ tmdSubmit_button.grid(row=6, column=0, columnspan=2, pady=1)
 
 tmdExit_button.grid(row=8, column=0, columnspan=2, pady=2)
 
-#--
+#-- progress bar
+
 progress_label = tkinter.Label(tmdApp, text="Progress Bar!", background=LightPink)
 progress_label.grid(row=9, column=0)
 progress = ttk.Progressbar(tmdApp, length=100)
 progress.grid(row=10, column=0, columnspan=2, pady=1)
-
-
 
 tmdApp.mainloop()
